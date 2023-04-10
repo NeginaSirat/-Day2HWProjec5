d004e07f912d4928ae66777ce74886e06a8eb78a
hedas <hedas@10.0.0.133>
2023-04-10 10:55:57 -0700

    Added test()

--- src/day2Package5/Day2Test5.java
+++ src/day2Package5/Day2Test5.java
@@ -4,5 +4,10 @@ public class Day2Test5 {
 	public static void main(String[] args) {
 		System.out.println("testing.....");
 	}
+	
+	public void test5() {
+		System.out.println("my stash test");
+	}
+	
 
 }
